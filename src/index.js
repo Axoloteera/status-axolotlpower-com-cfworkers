ddb6ba630663a16f6f39507a1218fed293cf60c6
Gtd232 <im.gtd232@outlook.com>
2024-01-30 14:21:34 +0800

    支持了网站的检测

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	// The scheduled handler is invoked at the interval set in our wrangler.toml's
-	// [[triggers]] configuration.
 	async scheduled(event, env, ctx) {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
-
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+		const config = JSON.parse(await env.STATUS_INFO.get('CONFIG'));
+		let data = { monitors: {}, date: new Date().getTime(), notice: config['notice'] };
+		//console.log(config);
+		for (let i of config['monitors']) {
+			console.log(i);
+			if (i['type'] == 'website') {
+				await fetch(i['url'], { method: i['method'] })
+					.then((response) => {
+						if (response.status == 200) {
+							//console.log("Website is up!1");
+							data['monitors'][i['name']] = true;
+						} else {
+							//console.log("Website is down!2");
+							data['monitors'][i['name']] = false;
+						}
+					})
+					.catch(() => {
+						//console.log("Website is down!3");
+						data['monitors'][i['name']] = false;
+					});
+			}
+		}
+		//console.log(data);
+		await env.STATUS_INFO.put('DATA', JSON.stringify(data));
 	},
 };
